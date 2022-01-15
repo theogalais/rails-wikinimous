adc4c0ba4607a0458fca303b40498d9b32b23ef8
theo galais <theogalais@gmail.com>
2022-01-15 17:04:20 +0100

    wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+10.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph
+  )
+end
